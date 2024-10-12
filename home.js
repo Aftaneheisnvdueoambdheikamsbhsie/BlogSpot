5da91290d4e1ccfa45a3dd96a7b44f3b07bf3fb2
aftacreative <aftaam6@gmail.com>
2024-10-13 06:00:30 +0700

    Update home.js

--- home.js
+++ home.js
@@ -5,16 +5,14 @@ document.addEventListener('DOMContentLoaded', function() {
     const burgerIcon = document.querySelector('.burger-icon');
 
     function toggleMenu() {
-        menu.classList.toggle('hide-menu'); // Toggle visibility of the menu
-        menu.classList.toggle('show-menu'); // Show menu if hidden
-
-        // Change icon and label based on menu state
         if (menu.classList.contains('hide-menu')) {
-            burgerIcon.innerHTML = '&#9776;';  // Change back to burger
-            menuLabel.innerText = 'Menu';
-        } else {
+            menu.classList.remove('hide-menu');
             burgerIcon.innerHTML = '&#10006;';  // Change to X
             menuLabel.innerText = 'Close';
+        } else {
+            menu.classList.add('hide-menu');
+            burgerIcon.innerHTML = '&#9776;';  // Change back to burger
+            menuLabel.innerText = 'Menu';
         }
     }
 
