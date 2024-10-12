6826d18b32c36f8f5d6c85037dfc01161755a383
aftacreative <aftaam6@gmail.com>
2024-10-13 05:09:49 +0700

    Update home.js

--- home.js
+++ home.js
@@ -2,17 +2,17 @@ document.addEventListener('DOMContentLoaded', function() {
     const burger = document.querySelector('#burger-menu');
     const menu = document.getElementById('menu');
     const menuLabel = document.getElementById('menu-label');
-    const burgerIcon = document.querySelector('.burger-icon');
+    const burgerLines = document.querySelectorAll('.line');
 
     function toggleMenu() {
-        if (menu.classList.contains('hide-menu')) {
-            menu.classList.remove('hide-menu');
-            burgerIcon.innerHTML = '&#10006;';  // Change to X
-            menuLabel.innerText = 'Close';
-        } else {
-            menu.classList.add('hide-menu');
-            burgerIcon.innerHTML = '&#9776;';  // Change back to burger
+        if (menu.classList.contains('show-menu')) {
+            menu.classList.remove('show-menu');
+            burger.classList.remove('active'); // Remove "x"
             menuLabel.innerText = 'Menu';
+        } else {
+            menu.classList.add('show-menu');
+            burger.classList.add('active'); // Turn burger into "x"
+            menuLabel.innerText = 'Close';
         }
     }
 
