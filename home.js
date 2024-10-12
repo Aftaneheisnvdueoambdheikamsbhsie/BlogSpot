c137d9bc3b4687ec43ffb5b3086af9e35990f9cb
aftacreative <aftaam6@gmail.com>
2024-10-13 04:35:48 +0700

    Update home.js

--- home.js
+++ home.js
@@ -1,15 +1,20 @@
-function toggleMenu() {
-    const menu = document.getElementById('menu');
-    const menuLabel = document.getElementById('menu-label');
-    const burgerIcon = document.querySelector('.burger-icon');
+document.addEventListener('DOMContentLoaded', function() {
+    function toggleMenu() {
+        const menu = document.getElementById('menu');
+        const menuLabel = document.getElementById('menu-label');
+        const burgerIcon = document.querySelector('.burger-icon');
 
-    if (menu.classList.contains('hide-menu')) {
-        menu.classList.remove('hide-menu');
-        burgerIcon.innerHTML = '&#10006;';  // Change burger to 'X'
-        menuLabel.innerText = 'Close';
-    } else {
-        menu.classList.add('hide-menu');
-        burgerIcon.innerHTML = '&#9776;';  // Change 'X' back to burger
-        menuLabel.innerText = 'Menu';
+        if (menu.classList.contains('hide-menu')) {
+            menu.classList.remove('hide-menu');
+            burgerIcon.innerHTML = '&#10006;';  // Change burger to 'X'
+            menuLabel.innerText = 'Close';
+        } else {
+            menu.classList.add('hide-menu');
+            burgerIcon.innerHTML = '&#9776;';  // Change 'X' back to burger
+            menuLabel.innerText = 'Menu';
+        }
     }
-}
+
+    // Attach the toggleMenu function to burger click
+    document.querySelector('.burger').addEventListener('click', toggleMenu);
+});
