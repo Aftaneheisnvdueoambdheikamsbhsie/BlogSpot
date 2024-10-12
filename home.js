79ec6550e2b484073076a60d17d264525bd43d89
aftacreative <aftaam6@gmail.com>
2024-10-13 04:50:26 +0700

    Update home.js

--- home.js
+++ home.js
@@ -1,20 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
-    function toggleMenu() {
-        const menu = document.getElementById('menu');
-        const menuLabel = document.getElementById('menu-label');
-        const burgerIcon = document.querySelector('.burger-icon');
+    const burger = document.querySelector('#burger-menu');
+    const menu = document.getElementById('menu');
+    const menuLabel = document.getElementById('menu-label');
+    const burgerIcon = document.querySelector('.burger-icon');
 
+    function toggleMenu() {
         if (menu.classList.contains('hide-menu')) {
             menu.classList.remove('hide-menu');
-            burgerIcon.innerHTML = '&#10006;';  // Change burger to 'X'
+            burgerIcon.innerHTML = '&#10006;';  // Change to X
             menuLabel.innerText = 'Close';
         } else {
             menu.classList.add('hide-menu');
-            burgerIcon.innerHTML = '&#9776;';  // Change 'X' back to burger
+            burgerIcon.innerHTML = '&#9776;';  // Change back to burger
             menuLabel.innerText = 'Menu';
         }
     }
 
-    // Attach the toggleMenu function to burger click
-    document.querySelector('.burger').addEventListener('click', toggleMenu);
+    burger.addEventListener('click', toggleMenu);
 });
