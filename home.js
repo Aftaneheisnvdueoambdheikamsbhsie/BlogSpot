d9edf61ce994c7329ddf7b0b347236a2c2b232fc
aftacreative <aftaam6@gmail.com>
2024-10-13 05:39:12 +0700

    Update home.js

--- home.js
+++ home.js
@@ -1,18 +1,10 @@
-// home.js
-document.addEventListener('DOMContentLoaded', function () {
-    const burgerMenu = document.getElementById('burger-menu');
-    const menu = document.getElementById('menu');
-    
-    burgerMenu.addEventListener('click', function() {
-        burgerMenu.classList.toggle('active');
-        menu.classList.toggle('show-menu');
-        
-        // Ubah label dari Menu ke Close
-        const menuLabel = document.getElementById('menu-label');
-        if (burgerMenu.classList.contains('active')) {
-            menuLabel.textContent = 'Close';
-        } else {
-            menuLabel.textContent = 'Menu';
-        }
-    });
+// Toggle menu functionality
+const burgerMenu = document.getElementById('burger-menu');
+const menu = document.getElementById('menu');
+
+burgerMenu.addEventListener('click', function() {
+    burgerMenu.classList.toggle('active');
+    menu.classList.toggle('show-menu');
+    const menuLabel = document.getElementById('menu-label');
+    menuLabel.textContent = menu.classList.contains('show-menu') ? 'Close' : 'Menu';
 });
