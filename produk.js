b402c7b703e7e80fb5663b75d3d604ad18652bfb
aftacreative <aftaam6@gmail.com>
2024-10-13 10:56:43 +0700

    Update produk.js

--- produk.js
+++ produk.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
+    // Scroll Menu
     const menuItems = document.getElementById('menu-items');
     const scrollRightBtn = document.getElementById('scroll-right');
     const scrollLeftBtn = document.getElementById('scroll-left');
@@ -9,5 +10,17 @@ document.addEventListener('DOMContentLoaded', function() {
 
     scrollLeftBtn.addEventListener('click', function() {
         menuItems.scrollBy({ left: -100, behavior: 'smooth' });
+    });
+
+    // Burger Menu untuk memunculkan sidebar
+    const burgerMenu = document.querySelector('.burger-icon');
+    const sidebarMenu = document.getElementById('sidebar-menu');
+
+    burgerMenu.addEventListener('click', function() {
+        if (sidebarMenu.style.display === 'flex') {
+            sidebarMenu.style.display = 'none';
+        } else {
+            sidebarMenu.style.display = 'flex';
+        }
     });
 });
