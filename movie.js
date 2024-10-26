392fbf3890c50a4b90506c7cadcd79b1f116c6e4
aftacreative <aftaam6@gmail.com>
2024-10-26 18:31:06 +0700

    Update movie.js

--- movie.js
+++ movie.js
@@ -1,6 +1,8 @@
 let currentSlide = 0;
 const slides = document.querySelectorAll('.carousel-item');
 const totalSlides = slides.length;
+const burgerMenu = document.getElementById('burgerMenu');
+const navLinks = document.getElementById('navLinks');
 
 document.querySelector('.next').addEventListener('click', () => {
     changeSlide(1);
@@ -9,9 +11,22 @@ document.querySelector('.next').addEventListener('click', () => {
 document.querySelector('.prev').addEventListener('click', () => {
     changeSlide(-1);
 });
-
 function changeSlide(direction) {
     slides[currentSlide].classList.remove('active');
     currentSlide = (currentSlide + direction + totalSlides) % totalSlides;
     slides[currentSlide].classList.add('active');
 }
+
+// Show/hide menu on burger icon click
+burgerMenu.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+
+// Close menu when clicking outside of it
+window.addEventListener('click', (event) => {
+    if (!burgerMenu.contains(event.target) && !navLinks.contains(event.target)) {
+        navLinks.classList.remove('active');
+    }
+});
+
+
